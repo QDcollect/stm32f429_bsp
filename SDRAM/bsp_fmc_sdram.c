2518ffa88e05fbc7b111b11f995c3cbe7a8bd6b8
QDcollect <124479945+QDcollect@users.noreply.github.com>
2023-03-10 22:50:15 +0800

    Add files via upload

---
+++ SDRAM/bsp_fmc_sdram.c
@@ -0,0 +1,223 @@
+/*
+*********************************************************************************************************
+*	主要功能：
+*			实现FMC扩展SDRAM的初始化
+*	使用条件：
+*			1、需要在main.h调用 #include "bsp_fmc_sdram.h"
+*			2、需要在主函数中调用		SDRAM_Initialization(); 同时屏蔽MX_FMC_Init();
+*			3、检查相应的初始化引脚
+*				于MX自动生成的不同的引脚分别是：GPIOH 之中的PIN_2|3|5
+*	
+*********************************************************************************************************
+*/
+#include "main.h"
+
+static void SDRAM_Initialization_Sequence(SDRAM_HandleTypeDef *hsdram, FMC_SDRAM_CommandTypeDef *Command);
+
+
+//删除main里面的MX_FMC_Init();
+//hsdram1给扩大一下 不用 人家帮你做好了
+void SDRAM_Initialization(void)
+{
+		MX_FMC_Init();
+		FMC_SDRAM_CommandTypeDef command = {0};
+		SDRAM_Initialization_Sequence(&hsdram1, &command);//这个程序的作用就是对这个COMMAND进行操作，哦这个程序里面有
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: SDRAM初始化序列
+*	功能说明: 完成SDRAM序列初始化
+*	形    参: hsdram: SDRAM句柄
+*			  Command: 命令结构体指针
+*	返 回 值: None
+*********************************************************************************************************
+*/
+static void SDRAM_Initialization_Sequence(SDRAM_HandleTypeDef *hsdram, FMC_SDRAM_CommandTypeDef *Command)
+{
+	__IO uint32_t tmpmrd =0;
+ 
+    /*##-1- 时钟使能命令 ##################################################*/
+	Command->CommandMode = FMC_SDRAM_CMD_CLK_ENABLE;
+	Command->CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;;
+	Command->AutoRefreshNumber = 1;
+	Command->ModeRegisterDefinition = 0;
+
+	/* 发送命令 */
+	HAL_SDRAM_SendCommand(hsdram, Command, SDRAM_TIMEOUT);
+
+    /*##-2- 插入延迟，至少100us ##################################################*/
+	HAL_Delay(1);
+
+    /*##-3- 整个SDRAM预充电命令，PALL(precharge all) #############################*/
+	Command->CommandMode = FMC_SDRAM_CMD_PALL;
+	Command->CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
+	Command->AutoRefreshNumber = 1;
+	Command->ModeRegisterDefinition = 0;
+
+	/* 发送命令 */
+	HAL_SDRAM_SendCommand(hsdram, Command, SDRAM_TIMEOUT);
+
+    /*##-4- 自动刷新命令 #######################################################*/
+	Command->CommandMode = FMC_SDRAM_CMD_AUTOREFRESH_MODE;
+	Command->CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
+	Command->AutoRefreshNumber = 8;
+	Command->ModeRegisterDefinition = 0;
+
+	/* 发送命令 */
+	HAL_SDRAM_SendCommand(hsdram, Command, SDRAM_TIMEOUT);
+
+    /*##-5- 配置SDRAM模式寄存器 ###############################################*/
+	tmpmrd = (uint32_t)SDRAM_MODEREG_BURST_LENGTH_1          |
+					 SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL   |
+					 SDRAM_MODEREG_CAS_LATENCY_3           |
+					 SDRAM_MODEREG_OPERATING_MODE_STANDARD |
+					 SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;
+
+	Command->CommandMode = FMC_SDRAM_CMD_LOAD_MODE;
+	Command->CommandTarget = FMC_SDRAM_CMD_TARGET_BANK1;
+	Command->AutoRefreshNumber = 1;
+	Command->ModeRegisterDefinition = tmpmrd;
+
+	/* 发送命令 */
+	HAL_SDRAM_SendCommand(hsdram, Command, SDRAM_TIMEOUT);
+
+    /*##-6- 设置自刷新率 ####################################################*/
+    /*
+        SDRAM refresh period / Number of rows）*SDRAM时钟速度 – 20
+      = 64ms / 4096 *84MHz - 20
+      = 1292.5 取值1293
+    */
+	HAL_SDRAM_ProgramRefreshRate(hsdram, REFRESH_COUNT); 
+}
+
+
+
+
+/*
+*********************************************************************************************************
+*	函 数 名: bsp_TestExtSDRAM
+*	功能说明: 扫描测试外部SDRAM的全部单元。
+*	形    参: 无
+*	返 回 值: 0 表示测试通过； 大于0表示错误单元的个数。
+*********************************************************************************************************
+*/
+uint32_t bsp_TestExtSDRAM1(void)
+{
+	uint32_t i;
+	uint32_t *pSRAM;
+	uint8_t *pBytes;
+	uint32_t err;
+	const uint8_t ByteBuf[4] = {0x55, 0xA5, 0x5A, 0xAA};
+
+	/* 写SRAM */
+	pSRAM = (uint32_t *)EXT_SDRAM_ADDR;
+	for (i = 0; i < EXT_SDRAM_SIZE / 4; i++)
+	{
+		*pSRAM++ = i;
+	}
+
+	/* 读SRAM */
+	err = 0;
+	pSRAM = (uint32_t *)EXT_SDRAM_ADDR;
+	for (i = 0; i < EXT_SDRAM_SIZE / 4; i++)
+	{
+		if (*pSRAM++ != i)
+		{
+			err++;
+		}
+	}
+
+	if (err >  0)
+	{
+		return  (4 * err);
+	}
+
+	/* 对SRAM 的数据求反并写入 */
+	pSRAM = (uint32_t *)EXT_SDRAM_ADDR;
+	for (i = 0; i < EXT_SDRAM_SIZE / 4; i++)
+	{
+		*pSRAM = ~*pSRAM;
+		pSRAM++;
+	}
+
+	/* 再次比较SDRAM的数据 */
+	err = 0;
+	pSRAM = (uint32_t *)EXT_SDRAM_ADDR;
+	for (i = 0; i < EXT_SDRAM_SIZE / 4; i++)
+	{
+		if (*pSRAM++ != (~i))
+		{
+			err++;
+		}
+	}
+
+	if (err >  0)
+	{
+		return (4 * err);
+	}
+
+	/* 测试按字节方式访问, 目的是验证 FSMC_NBL0 、 FSMC_NBL1 口线 */
+	pBytes = (uint8_t *)EXT_SDRAM_ADDR;
+	for (i = 0; i < sizeof(ByteBuf); i++)
+	{
+		*pBytes++ = ByteBuf[i];
+	}
+
+	/* 比较SDRAM的数据 */
+	err = 0;
+	pBytes = (uint8_t *)EXT_SDRAM_ADDR;
+	for (i = 0; i < sizeof(ByteBuf); i++)
+	{
+		if (*pBytes++ != ByteBuf[i])
+		{
+			err++;
+		}
+	}
+	if (err >  0)
+	{
+		return err;
+	}
+	return 0;
+}
+
+
+/*
+*********************************************************************************************************
+*	函 数 名: ReadWriteTest
+*	功能说明: 读数据，并打印出来
+*	形    参：无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+void ReadWriteTest(void)
+{
+	uint32_t i;
+	uint32_t *pBuf;
+	
+
+	/* 写入测试数据0xAAAA5555 */
+	pBuf = (uint32_t *)(EXT_SDRAM_ADDR + TEST_ADDRESS);
+	for (i = 0; i < TEST_BUF_SIZE; i++)		
+	{
+		pBuf[i] = 0xAAAA5555;
+	}
+	
+	printf("物理地址：0x%08X  大小: %d字节  显示: %d字节  数据如下: \r\n", EXT_SDRAM_ADDR + TEST_ADDRESS, EXT_SDRAM_SIZE, TEST_BUF_SIZE*4);
+	
+	/* 打印数据 */
+	pBuf = (uint32_t *)(EXT_SDRAM_ADDR + TEST_ADDRESS);
+	for (i = 0; i < TEST_BUF_SIZE; i++)
+	{
+		printf(" %04X", pBuf[i]);
+
+		if ((i & 7) == 7)
+		{
+			printf("\r\n");		/* 每行显示32字节数据 */
+		}
+		else if ((i & 7) == 3)
+		{
+			printf(" - ");
+		}
+	}
+}
---
+++ SDRAM/bsp_fmc_sdram.h
@@ -0,0 +1,35 @@
+
+/* #define SDRAM_MEMORY_WIDTH            FMC_SDRAM_MEM_BUS_WIDTH_8  */
+/* #define SDRAM_MEMORY_WIDTH            FMC_SDRAM_MEM_BUS_WIDTH_16 */
+#define SDRAM_MEMORY_WIDTH               FMC_SDRAM_MEM_BUS_WIDTH_32
+
+#define SDCLOCK_PERIOD                   FMC_SDRAM_CLOCK_PERIOD_2
+/* #define SDCLOCK_PERIOD                FMC_SDRAM_CLOCK_PERIOD_3 */
+
+#define SDRAM_TIMEOUT                    ((uint32_t)0xFFFF)
+#define REFRESH_COUNT                    ((uint32_t)1293)    /* SDRAM自刷新计数 */  
+
+/* SDRAM的参数配置 */
+#define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
+#define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
+#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
+#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
+#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
+#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
+#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
+
+
+#define EXT_SDRAM_ADDR  	((uint32_t)0xC0000000)
+#define EXT_SDRAM_SIZE		(16 * 1024 * 1024)
+
+
+#define TEST_ADDRESS	0    /* 从地址0开始测试 */
+#define TEST_BUF_SIZE	256  /* 测试缓冲大小 */
+
+void SDRAM_Initialization(void);
+uint32_t bsp_TestExtSDRAM1(void);
+void ReadWriteTest(void);
